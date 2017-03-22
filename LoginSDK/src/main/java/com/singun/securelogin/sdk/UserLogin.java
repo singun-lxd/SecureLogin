ed178ccb04acd65a8336c5efafea9c0088487ed4
singun-lxd <liuxindian@gmail.com>
2017-03-22 13:42:48 +0800

    + add normal projects

---
+++ LoginSDK/src/main/java/com/singun/securelogin/sdk/UserLogin.java
@@ -0,0 +1,8 @@
+package com.singun.securelogin.sdk;
+
+/**
+ * Created by singun on 2017/3/22 0022.
+ */
+
+public class UserLogin {
+}
---
+++ LoginServer/src/main/java/com/singun/securelogin/server/ServerMainActivity.java
@@ -0,0 +1,18 @@
+package com.singun.securelogin.server;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.singun.securelogin.R;
+
+/**
+ * Created by singun on 2017/3/22 0022.
+ */
+
+public class ServerMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
